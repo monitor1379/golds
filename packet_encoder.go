c474f80988ede97f44c02e94d95d2bdcb5802be4
monitor1379 <yy4f5da2@hotmail.com>
2020-04-27 19:22:24 +0800

    MOD

--- packet_encoder.go
+++ packet_encoder.go
@@ -7,9 +7,111 @@
 
 package golds
 
-type PacketEncoder struct{}
+import (
+	"bufio"
+	"io"
+	"strconv"
+)
 
-func NewPacketEncoder() *PacketEncoder {
-	packetEncoder := new(PacketEncoder)
-	return packetEncoder
+type PacketEncoder struct {
+	writer *bufio.Writer
+}
+
+func NewPacketEncoder(writer io.Writer) *PacketEncoder {
+	return &PacketEncoder{
+		writer: bufio.NewWriter(writer),
+	}
+}
+
+func (this *PacketEncoder) Encode(packet *Packet) error {
+	return this.encode(packet)
+}
+
+func (this *PacketEncoder) encode(packet *Packet) error {
+	var err error
+	switch packet.PacketType {
+	case PacketTypeString, PacketTypeError, PacketTypeInt:
+		err = this.encodeBytes(packet)
+	case PacketTypeBulkString:
+		err = this.encodeBulkBytes(packet)
+	case PacketTypeArray:
+		err = this.encodeArray(packet)
+	default:
+		return ErrInvalidPacketType
+	}
+
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (this *PacketEncoder) encodeBytes(packet *Packet) error {
+	defer this.writer.Flush()
+
+	err := this.writer.WriteByte(byte(packet.PacketType))
+	if err != nil {
+		return err
+	}
+
+	_, err = this.writer.Write(packet.Value)
+	if err != nil {
+		return err
+	}
+
+	err = this.writer.WriteByte('\n')
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (this *PacketEncoder) encodeBulkBytes(packet *Packet) error {
+	defer this.writer.Flush()
+
+	err := this.writer.WriteByte(byte(packet.PacketType))
+	if err != nil {
+		return err
+	}
+
+	if packet.Value == nil {
+		_, err = this.writer.WriteString("-1")
+		if err != nil {
+			return err
+		}
+
+		err = this.writer.WriteByte('\n')
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+
+	_, err = this.writer.WriteString(strconv.Itoa(len(packet.Value)))
+	if err != nil {
+		return err
+	}
+
+	err = this.writer.WriteByte('\n')
+	if err != nil {
+		return err
+	}
+
+	_, err = this.writer.Write(packet.Value)
+	if err != nil {
+		return err
+	}
+
+	err = this.writer.WriteByte('\n')
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (this *PacketEncoder) encodeArray(packet *Packet) error {
+	defer this.writer.Flush()
+	return nil
 }
--- packet_encoder_test.go
+++ packet_encoder_test.go
@@ -7,8 +7,37 @@ package golds_test
  * @Last Modified time: 2020-04-26 16:47:31
  */
 
-import "testing"
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/monitor1379/golds"
+)
 
 func TestPacketEncoder(t *testing.T) {
+	packets := []*golds.Packet{
+		&golds.Packet{PacketType: golds.PacketTypeString, Value: []byte("hello")},
+		&golds.Packet{PacketType: golds.PacketTypeString, Value: []byte("hello world")},
+		&golds.Packet{PacketType: golds.PacketTypeString, Value: []byte("")},
+		&golds.Packet{PacketType: golds.PacketTypeError, Value: []byte("Error message")},
+		&golds.Packet{PacketType: golds.PacketTypeBulkString, Value: []byte("hello")},
+		&golds.Packet{PacketType: golds.PacketTypeBulkString, Value: []byte("hello world")},
+		&golds.Packet{PacketType: golds.PacketTypeBulkString, Value: []byte("")},
+		&golds.Packet{PacketType: golds.PacketTypeBulkString, Value: nil},
+		&golds.Packet{PacketType: golds.PacketTypeBulkString, Value: []byte("hello\nworld")},
+		&golds.Packet{PacketType: golds.PacketTypeBulkString, Value: []byte("hello\nworld\n")},
+	}
 
+	for _, packet := range packets {
+		buf := bytes.Buffer{}
+		packetEncoder := golds.NewPacketEncoder(&buf)
+		err := packetEncoder.Encode(packet)
+		if err != nil {
+			t.Errorf("ERROR: encode error: %s", err)
+			continue
+		}
+		fmt.Println(strconv.Quote(buf.String()))
+	}
 }
